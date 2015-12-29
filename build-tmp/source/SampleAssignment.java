fc88cc4586ee12c524ddd3e5be66ba7e9ee3f310
Alexandria893 <Alexandriabrown893@gmail.com>
2015-12-28 16:31:44 -0800

    idk

---
+++ build-tmp/source/SampleAssignment.java
@@ -0,0 +1,37 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SampleAssignment extends PApplet {
+  public void setup() {
+
+fill(255,255,0);
+ellipse(100,50,80,80);
+arc(100,50,60,60,PI/8,7*PI/8);
+fill(0,0,0);
+ellipse(85,40,10,15);
+ellipse(115,40,10,15);
+
+    noLoop();
+  }
+
+  public void settings() { size(200,100); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SampleAssignment" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
